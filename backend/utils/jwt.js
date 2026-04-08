6ce76c9f436f64e2191101ab306b49a5e7aa794d
Rishith88 <rishithmanchala989@gmail.com>
2026-04-08 15:59:35 +0530

    Delete backend directory

--- backend/utils/jwt.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { env } from '../config/env.js';
-
-export const signToken = (payload) =>
-  jwt.sign(payload, env.JWT_SECRET, {
-    expiresIn: '7d',
-  });
-
-export const verifyToken = (token) => jwt.verify(token, env.JWT_SECRET);
