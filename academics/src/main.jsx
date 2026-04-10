bded08b45de6025cfe46931b9bc01076913de70b
Navaneeth19 <1117@cornerstoneschool.edu.in>
2026-04-10 20:43:21 +0530

    fix: add BrowserRouter basename from BASE_URL so /login routes work on GitHub Pages refresh

--- academics/src/main.jsx
+++ academics/src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+const basename = import.meta.env.BASE_URL?.replace(/\/$/, '') || '';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
@@ -17,4 +19,4 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.getRegistrations().then(registrations => {
     registrations.forEach(r => r.unregister());
   });
-}
+}
