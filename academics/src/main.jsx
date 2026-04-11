72e6a68276136ae8e0be10cdaafb15964e04b986
Navaneeth19 <1117@cornerstoneschool.edu.in>
2026-04-11 10:03:05 +0530

    fix: HashRouter eliminates 404 on refresh, fix YAML syntax, bright landing page via python heredoc

--- academics/src/main.jsx
+++ academics/src/main.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const basename = '';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
