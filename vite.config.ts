2125da9afbc6d01ab49ff909268001d575f2ee9a
rishithmanchala989 <rishithmanchala989@gmail.com>
2026-05-29 10:03:20 +0000

    fix page crash

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/OP-CS_CONNECT/',
   plugins: [react()],
   resolve: {
     alias: {
