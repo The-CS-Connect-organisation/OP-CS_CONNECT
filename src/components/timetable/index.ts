0f4cb8a479f622300124dfb1407ff4d4984da646
Ganesh <ganesh@users.noreply.github.com>
2026-07-08 22:16:48 +0530

    Fix: export SubjectTeacherMap from timetable index

--- src/components/timetable/index.ts
+++ src/components/timetable/index.ts
@@ -2,5 +2,5 @@ export { TimetableView } from './TimetableView';
 export { GridView } from './GridView';
 export { KanbanView } from './KanbanView';
 export { BulkWizard } from './BulkWizard';
-export type { TimetableEntry, ViewMode, DropdownOption, CRUDHandlers, TimetableSlot } from './types';
+export type { TimetableEntry, ViewMode, DropdownOption, CRUDHandlers, TimetableSlot, SubjectTeacherMap } from './types';
 export { DAYS, DAY_LABELS, DEFAULT_PERIODS } from './types';
